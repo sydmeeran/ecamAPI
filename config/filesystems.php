bec60bd2ef16f2d424440f55da1ce0de5eba1e1c
naingminkhant <naingminkhant.cm@gmail.com>
2018-12-17 17:37:39 +0630

    nothing

--- config/filesystems.php
+++ config/filesystems.php
@@ -74,11 +74,11 @@ return [
         ],
         'gcs'        => [
             'driver'          => 'gcs',
-            'project_id'      => env('GOOGLE_CLOUD_PROJECT_ID', 'layla-123'),
+            'project_id'      => env('GOOGLE_CLOUD_PROJECT_ID'),
             'key_file'        => env('GOOGLE_CLOUD_KEY_FILE', storage_path('app/google/service-account-key.json')),
-            'bucket'          => env('GOOGLE_CLOUD_STORAGE_BUCKET', 'layla-123.appspot.com'),
+            'bucket'          => env('GOOGLE_CLOUD_STORAGE_BUCKET'),
             'path_prefix'     => env('GOOGLE_CLOUD_STORAGE_PATH_PREFIX', null),
-            'storage_api_uri' => env('GOOGLE_CLOUD_STORAGE_API_URI', 'https://storage.googleapis.com/layla-123.appspot.com/'),
+            'storage_api_uri' => env('GOOGLE_CLOUD_STORAGE_API_URI'),
         ],
     ],
 ];
