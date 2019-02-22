c900f3133862231e5e5044269746e5d67b03b7d8
lwinht <lwinhtetthu8597@gmail.com>
2019-02-22 11:40:13 +0630

    delete button & quotation yearly value input

--- database/seeds/PermissionSeeder.php
+++ database/seeds/PermissionSeeder.php
@@ -12,85 +12,85 @@ class PermissionSeeder extends Seeder
      */
     public function run()
     {
-        // $permission = ['create', 'retrieve', 'update', 'delete'];
+        $permission = ['create', 'retrieve', 'update', 'delete'];
 
-        // Permission::create([
-        //     'permission' => 'all'
-        // ]);
+        Permission::create([
+            'permission' => 'all'
+        ]);
 
-        // foreach($permission as $p){
-        //     Permission::create([
-        //         'permission' => 'role-'.$p
-        //     ]);
-        // }
+        foreach($permission as $p){
+            Permission::create([
+                'permission' => 'role-'.$p
+            ]);
+        }
 
-        // foreach($permission as $p){
-        //     Permission::create([
-        //         'permission' => 'user-'.$p
-        //     ]);
-        // }
+        foreach($permission as $p){
+            Permission::create([
+                'permission' => 'user-'.$p
+            ]);
+        }
 
-        // Permission::create([
-        //     'permission' => 'user-deactive'
-        // ]);
+        Permission::create([
+            'permission' => 'user-deactive'
+        ]);
 
-        // foreach($permission as $p){
-        //     Permission::create([
-        //         'permission' => 'customer-'.$p
-        //     ]);
-        // }
+        foreach($permission as $p){
+            Permission::create([
+                'permission' => 'customer-'.$p
+            ]);
+        }
 
-        // Permission::create([
-        //     'permission' => 'customer-deactive'
-        // ]);
+        Permission::create([
+            'permission' => 'customer-deactive'
+        ]);
 
-        // Permission::create([
-        //     'permission' => 'customer-suspend'
-        // ]);
+        Permission::create([
+            'permission' => 'customer-suspend'
+        ]);
 
-        // foreach($permission as $p){
-        //     Permission::create([
-        //         'permission' => 'job-entry-'.$p
-        //     ]);
-        // }
+        foreach($permission as $p){
+            Permission::create([
+                'permission' => 'job-entry-'.$p
+            ]);
+        }
 
-        // foreach($permission as $p){
-        //     Permission::create([
-        //         'permission' => 'quotation-'.$p
-        //     ]);
-        // }
+        foreach($permission as $p){
+            Permission::create([
+                'permission' => 'quotation-'.$p
+            ]);
+        }
 
         Permission::create([
                 'permission' => 'quotation-cancel'
             ]);
 
-        // Permission::create([
-        //     'permission' => 'invoice-retrieve'
-        // ]);
-        // Permission::create([
-        //     'permission' => 'invoice-create'
-        // ]);
-        // Permission::create([
-        //     'permission' => 'invoice-delete'
-        // ]);
+        Permission::create([
+            'permission' => 'invoice-retrieve'
+        ]);
+        Permission::create([
+            'permission' => 'invoice-create'
+        ]);
+        Permission::create([
+            'permission' => 'invoice-delete'
+        ]);
 
-        // Permission::create([
-        //     'permission' => 'receipt-retrieve'
-        // ]);
-        // Permission::create([
-        //     'permission' => 'receipt-create'
-        // ]);
-        // Permission::create([
-        //     'permission' => 'receipt-delete'
-        // ]);
+        Permission::create([
+            'permission' => 'receipt-retrieve'
+        ]);
+        Permission::create([
+            'permission' => 'receipt-create'
+        ]);
+        Permission::create([
+            'permission' => 'receipt-delete'
+        ]);
 
-        // Permission::create([
-        //     'permission' => 'schedule-delete'
-        // ]);
+        Permission::create([
+            'permission' => 'schedule-delete'
+        ]);
 
-        // Permission::create([
-        //     'permission' => 'revenue-retrieve'
-        // ]);
+        Permission::create([
+            'permission' => 'revenue-retrieve'
+        ]);
 
     }
 }
